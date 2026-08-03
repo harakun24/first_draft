e14d61e8778ba2b254f0f41c0c49b005f9fb096d
dimas oppo <dimasoppo448@gmail.com>
2026-08-03 14:02:56 +0700

    push

--- asset/video.js
+++ asset/video.js
@@ -3,11 +3,23 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player("player", {
     width: "100%",
     height: "100%",
-    videoId: "oI50zFiXcpM"
+    // videoId: "oI50zFiXcpM"
+    videoId: "kkk_y1_C-80"
   })
 }
 
 function jumpTo(s) {
   player.seekTo(s, true)
   player.playVideo();
+  setTimeout(() => {
+    player.pauseVideo()
+    Swal.fire({
+      title: 'Jawab',
+      text: 'Do you want to continue',
+      icon: 'info',
+      confirmButtonText: 'Cool'
+    }).then(() => {
+      player.playVideo()
+    })
+  }, 10000)
 }
